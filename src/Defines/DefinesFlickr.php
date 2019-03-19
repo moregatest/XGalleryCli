973a69ed3f517af8ce7f3a72b317d28705af9087
Viet Vu <jooservices@gmail.com>
2019-03-19 16:09:04 +0700

    + Add image not found while try to get sizes

--- src/Defines/DefinesFlickr.php
+++ src/Defines/DefinesFlickr.php
@@ -15,10 +15,16 @@ namespace XGallery\Defines;
 class DefinesFlickr
 {
     /**
-     * Limit number of requests to get photo sizes
+     * Default limit number of requests to get photo sizes
      */
     const REST_LIMIT_PHOTOS_SIZE = 200;
+    /**
+     * Default limit downloads / execute
+     */
     const DOWNLOAD_LIMIT = 100;
+    /**
+     * Number photos to process resize
+     */
     const RESIZE_LIMIT = 100;
     const PHOTO_STATUS_DOWNLOADED = 1;
     const PHOTO_STATUS_ALREADY_DOWNLOADED = 2;
@@ -29,4 +35,5 @@ class DefinesFlickr
     const PHOTO_STATUS_ERROR_NOT_PHOTO = -2;
     const PHOTO_STATUS_ERROR_DOWNLOAD_FAILED = -3;
     const PHOTO_STATUS_ERROR_REDOWNLOAD_FAILED = -4;
+    const PHOTO_STATUS_ERROR_NOT_FOUND_GET_SIZES = -10;
 }
